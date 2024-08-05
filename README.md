c7bbd43386a15fcd36adc2f2550b84db86024963
RiaAyuP <mail.riaayupramudita@gmail.com>
2024-08-05 12:23:50 +0700

    added ollamachatbot.py

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-# Chat with GPT-4o Mini with Streamlit
+# Chat with LLMs with Streamlit
 
 Learning to use Streamlit, based on the LLM example uses provided in the references (with slight tweaks).
-- Chatbot `app.py` (please update `system` prompt for better experience)
+- GPT-4o Mini Chatbot `openaichatbot.py` (please update `system` prompt for better experience)
+- Open-Source LLM Chatbot using Ollama `ollamachatbot.py` (initially using Llama-3.1-8B)
 - File Q&A `fileqa.py`
 
 ## Techstack
-1. OpenAI LLM (GPT-4o-mini)
-2. Streamlit
+1. GPT-4o Mini LLM (OpenAI, proprietary)
+2. Llama 3.1 LLM (Meta, open-source)
+3. Streamlit
 
 ## References
 [Streamlit LLM Examples](https://llm-examples.streamlit.app/)
--- ollamachatbot.py
+++ ollamachatbot.py
@@ -1,14 +1,10 @@
 import os
 from dotenv import load_dotenv, find_dotenv
-from openai import OpenAI
+from ollama import chat
 import streamlit as st
 
-# Get the OpenAI API key
-_ = load_dotenv(find_dotenv())
-openai_api_key = os.getenv("OPENAI_API_KEY")
-
-st.title("💬 Dita's OpenAI Chatbot")
-st.caption("🚀 A Streamlit chatbot powered by OpenAI")
+st.title("💬 Dita's Ollama Chatbot")
+st.caption("🚀 A Streamlit chatbot powered by Ollama")
 if "messages" not in st.session_state:
     st.session_state["messages"] = [
         {"role": "system", "content": "You are a helpful assistant who only speaks in Indonesian."},
@@ -19,14 +15,11 @@ for msg in st.session_state.messages:
     st.chat_message(msg["role"]).write(msg["content"])
 
 if prompt := st.chat_input():
-    if not openai_api_key:
-        st.info("Please add your OpenAI API key to continue.")
-        st.stop()
     
-    client = OpenAI(api_key=openai_api_key)
     st.session_state.messages.append({"role": "user", "content": prompt})
     st.chat_message("user").write(prompt)
-    response = client.chat.completions.create(model="gpt-4o-mini", messages=st.session_state.messages)
-    msg = response.choices[0].message.content
+    response = ollama.chat(model="llama-3.1", messages=st.session_state.messages)
+    
+    msg = response['message']['content']
     st.session_state.messages.append({"role": "assistant", "content": msg})
     st.chat_message("assistant").write(msg)
