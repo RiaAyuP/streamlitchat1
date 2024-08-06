5b7ce8cfaaf0f81b405f85b22fbb09ea2b3b9263
RiaAyuP <mail.riaayupramudita@gmail.com>
2024-08-06 11:37:03 +0700

    added entry point file Hello.py

---
+++ Hello.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Dita's Home LLM",
+    page_icon="🏠",
+)
+
+st.write("# Dita's Home LLM 🏠")
+
+st.sidebar.success("Select a service above.")
+
+st.markdown(
+    """
+    SThis is the Home of Dita's personal and local use of various LLM.
+    Some are proprietary (OpenAI and Anthropic) and some are open-source through Ollama.
+    **👈 Select a service from the sidebar**
+    """
+)
